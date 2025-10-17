fabde2d3ec2d29a0dc89bae786c7a79a012f0cdf
sh1nek0 <annypodkaluk@gmail.com>
2025-10-17 22:44:24 +0000

    2 лабораторная

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,19 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	    if (!str) return;                 // на случай NULL
+    size_t i = 0, j = strlen(str);
+    if (j == 0) return;               // пустая строка
+    j--;                              // последний индекс (без '\0')
+
+    while (i < j) {
+        char tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+        i++;
+        j--;
+    }
 }
 
